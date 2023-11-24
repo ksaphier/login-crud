1125a9c8fc984f22bcdb1770ef2f8ad7b5924ba8
ksaphier <kevin.saphier@gmail.com>
2023-11-24 16:46:57 -0300

    conection mongodb

--- src/db.js
+++ src/db.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost/nombredelabasededatos?");
+    // Use the DATABASE_URL from the .env file
+    const dbUri = process.env.DATABASE_URL;
+    await mongoose.connect(dbUri);
     console.log("DB Connected");
   } catch (error) {
-    console.log(error);
+    console.error("Could not connect to DB: ", error);
   }
 };
